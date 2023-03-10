c68b98001b110df5611dd526c17aefd77cd3c215
Marius CA <marius@example.com>
2023-03-10 10:20:16 +0200

    users arr

---
+++ js/arrayOfObjects.js
@@ -0,0 +1,37 @@
+const users = [
+  {
+    name: 'Bob the Builder',
+    age: 45,
+    email: 'bob@builder.com',
+    isMarried: true,
+    isAdmin: false,
+  },
+  {
+    name: 'SpongeBob SquarePants',
+    age: 20,
+    email: 'spongebob@bikinibottom.com',
+    isMarried: false,
+    isAdmin: false,
+  },
+  {
+    name: 'Harry Potter',
+    age: 18,
+    email: 'harry@hogwarts.com',
+    isMarried: false,
+    isAdmin: true,
+  },
+  {
+    name: 'Winnie the Pooh',
+    age: 30,
+    email: 'winnie@hundredacrewood.com',
+    isMarried: false,
+    isAdmin: false,
+  },
+  {
+    name: 'Captain Hook',
+    age: 50,
+    email: 'captainhook@neverland.com',
+    isMarried: true,
+    isAdmin: false,
+  },
+];
