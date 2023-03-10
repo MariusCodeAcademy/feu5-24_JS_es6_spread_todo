480ae1de5cb98ed8742daab6c1b030d554df5310
Marius CA <marius@example.com>
2023-03-10 10:58:12 +0200

    todo magic

--- js/todo.js
+++ js/todo.js
@@ -7,9 +7,40 @@ const INIT_STATE = [
 
 const state = [...INIT_STATE];
 
+// taikomes
+const el = {
+  input: document.getElementById('new-todo-input'),
+  addBtn: document.getElementById('add-todo'),
+  list: document.getElementById('todos-list'),
+};
+
+// event listeners
+function initListeners() {
+  console.log('initListeners');
+  el.addBtn.addEventListener('click', addNewTodo);
+}
+
+app();
 function app() {
+  console.log('app started');
+  initListeners();
   // nupiesti/sugeneruoti todos
+  render();
   // prideti nauja todo
   // istrinti todo
   // atnaujinti todo
+}
+
+function addNewTodo() {}
+
+function render(arr = state) {
+  arr.forEach((tObj) => {
+    // make new li makeOneLi()
+    // add to el.list
+  });
+}
+
+function makeOneLi(todoObj) {
+  // <li class="list-group-item list-group-item-action">An item</li>
+  // sukurti ir grazinti li el
 }
