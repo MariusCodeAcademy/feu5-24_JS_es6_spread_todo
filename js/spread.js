ac51fff7c128108d3eb0f56558ecb1caf6243499
Marius CA <marius@example.com>
2023-03-10 09:58:42 +0200

    init

---
+++ js/spread.js
@@ -0,0 +1,31 @@
+'use strict';
+console.log('spread.js file was loaded');
+
+// ...array, ...object
+// operatorius kuris leidzia pernesti objekto ar masyvo reiksmes,
+// const a = [1,2,3]
+// ...a => 1, 2, 3
+
+/// Masyvai ==================
+
+const a = [1, 2, 3];
+const b = [4, 5, 6];
+
+//
+const aIrB = a.concat(b);
+console.log('aIrB ===', aIrB);
+
+const aIrBSpread = [0, ...a, 19, ...b, 7];
+console.log('aIrBSpread ===', aIrBSpread);
+
+const maxValue = Math.max(...aIrBSpread);
+console.log('maxValue ===', maxValue);
+
+console.log('spread', ...aIrBSpread);
+
+let bCopy = [...b];
+bCopy = [...bCopy, 25];
+console.log('bCopy ===', bCopy);
+console.log('b ===', b);
+
+// Object ============
