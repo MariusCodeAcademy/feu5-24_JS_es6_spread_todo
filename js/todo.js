79d039f64a0116db9ff36fd2890c2790631a8a61
Marius CA <marius@example.com>
2023-03-10 10:48:57 +0200

    html in place, bs5 added

---
+++ js/todo.js
@@ -0,0 +1,15 @@
+const INIT_STATE = [
+  { id: 't_001', title: 'Learn HTML', isDone: true },
+  { id: 't_002', title: 'Learn CSS', isDone: true },
+  { id: 't_003', title: 'Learn JS', isDone: false },
+  { id: 't_004', title: 'Learn GIT', isDone: false },
+];
+
+const state = [...INIT_STATE];
+
+function app() {
+  // nupiesti/sugeneruoti todos
+  // prideti nauja todo
+  // istrinti todo
+  // atnaujinti todo
+}
